e5ecd8320facb202ba1be02f2f385ce3a12bd3b3
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-04-29 11:07:05 +0200

    2904261107

--- Les10/scripts/pokeapi1.js
+++ Les10/scripts/pokeapi1.js
@@ -3,6 +3,8 @@
 // basis 'endpoint' van API opslaan in constante
 const apiUrl = "https://pokeapi.co/api/v2/";
 
+let pokelist;
+
 // fetch --> informatie ophalen uit correcte API endpoints
 fetch(apiUrl + "pokemon/")
     // 'antwoord' van de API call omzetten naar bruikbare JSON
@@ -19,21 +21,29 @@ fetch(apiUrl + "pokemon/")
         console.log(pokemonArray);
         // list div koppelen
         let divPokemonList = document.getElementById("pokemon-list");
-        // ordered list html element maken (ol)
-        let olPokemon = document.createElement("ol"); // <ol></ol>
-        let liPokemon = document.createElement("li"); // <li></li>
-        liPokemon.textContent = capitalise(pokemonArray[0].name); // <li>bulbasaur</li>
-        // li element IN ol steken
-        olPokemon.append(liPokemon); // <ol><li>bulbasaur</li></ol>
-        // ol element IN div steken
-        divPokemonList.append(olPokemon); // <div><ol><li>bulbasaur</li></ol></div>
+        // ol element IN div steken via function
+        divPokemonList.append(generateList(pokemonArray)); // <div><ol><li>bulbasaur</li></ol></div>
     })
     .catch(error => console.log(error.message));
 
-    // function om de eerste letter van een string
-    // om te zetten naar een hoofdletter
-    function capitalise(word) {
-        let firstLetter = word[0].toUpperCase();
-        let restOfWord = word.substring(1).toLowerCase(); // alle letters vanaf de tweede letter (index 1)
-        return firstLetter + restOfWord;
-    }
+// function om de eerste letter van een string
+// om te zetten naar een hoofdletter
+function capitalise(word) {
+    let firstLetter = word[0].toUpperCase();
+    let restOfWord = word.substring(1).toLowerCase(); // alle letters vanaf de tweede letter (index 1)
+    return firstLetter + restOfWord;
+}
+
+// function om een volledige <ol> lijst op te maken
+// met alle elementen uit een array
+function generateList(array) {
+    // ordered list html element maken (ol)
+    let olPokemon = document.createElement("ol"); // <ol></ol>
+    array.forEach(element => {
+        let liPokemon = document.createElement("li"); // <li></li>
+        liPokemon.textContent = capitalise(element.name); // <li>bulbasaur</li>
+        // li element IN ol steken
+        olPokemon.append(liPokemon); // <ol><li>bulbasaur</li></ol>
+    });
+    return olPokemon; // uiteindelijke resultaat (<ol><li>...)
+}
